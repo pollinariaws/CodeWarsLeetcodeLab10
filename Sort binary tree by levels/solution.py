306099e0e49ddf4d4107d9eae51a5e7b87cf862a
pollinariaws <polinazuzuk@gmail.com>
2026-04-17 19:49:47 +0300

    Create solution.py and directory
    created directory and added the solution

---
+++ Sort binary tree by levels/solution.py
@@ -0,0 +1,14 @@
+from collections import deque
+def tree_by_levels(node):
+    if node is None:
+        return []
+    result = []
+    queue = deque([node])
+    while queue:
+        current = queue.popleft()
+        result.append(current.value)
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return result
